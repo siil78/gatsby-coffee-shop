d728219ab7f6a47268a383924a84e14cf52d5bc1
Siil <jiri.jezek78@gmail.com>
2020-10-06 20:18:47 +0200

    about section

---
+++ src/components/Home/Info.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Link} from 'gatsby'
+import Title from '../Globals/Title'
+
+function Info() {
+    return (
+        <section className="py-5">
+            <div className="container">
+							<Title title="our story"></Title>
+	            <div className="row">
+                  <div className="col-10 col-sm-8 mx-auto text-center">
+                      <p className="lead text-muted mb-5">
+												Lorem ipsum dolor sit amet consectetur adipisicingelit. Expedita vitae laborum, eaque quaerat delectusvoluptatibus dicta nisi officiis iure, adipisciaccusantium. Soluta, a! Quae nesciunt quis,praesentium aliquid aliquam repellat non veniambeatae, iusto omnis fugit animi necessitatibusofficia recusandae, odit nam! Laboriosam repellendusamet soluta molestiae totam modi ea!
+                      </p>
+											<Link to="/about/">
+												<button className="btn text-uppercase btn-yellow">
+													about page
+												</button>
+											</Link>
+                  </div>
+              </div>
+            </div>
+        </section>
+    )
+}
+
+export default Info
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,33 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { Link, graphql } from "gatsby"
+//components
 import Layout from "../components/layout"
-
+import BackgroundSection from "../components/Globals/BackgroundSection"
 import SEO from "../components/seo"
+import Info from '../components/Home/Info'
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="regular joe's"
+      styleClass="default-background"
+    ></BackgroundSection>
+    <Info />
   </Layout>
 )
+
+export const query = graphql`
+  {
+    img: file(relativePath: {eq: "default-background.jpeg"}) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
