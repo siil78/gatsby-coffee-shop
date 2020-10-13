ebabf4f8e75173a467b283b99a10d2919451bad7
Siil <jiri.jezek78@gmail.com>
2020-10-13 02:15:01 +0200

    menu section version 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Layout from "../components/layout"
 import BackgroundSection from "../components/Globals/BackgroundSection"
 import SEO from "../components/seo"
 import Info from '../components/Home/Info'
+import Menu from '../components/Home/Menu'
 
 const IndexPage = ({data}) => (
   <Layout>
@@ -15,15 +16,36 @@ const IndexPage = ({data}) => (
       styleClass="default-background"
     ></BackgroundSection>
     <Info link="/about" buttonText="About" />
+    <Menu items={data.menu} />
   </Layout>
 )
 
+//použijeme page query
+//pro obrázek img a pro menu menu
 export const query = graphql`
   {
     img: file(relativePath: {eq: "default-background.jpeg"}) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    menu: allContentfulCoffeeItem {
+      edges {
+        node {
+          id
+          title
+          description {
+            description
+          }
+          price
+          category
+          image {
+            fixed(width: 50, height: 50) {
+              ...GatsbyContentfulFixed
+            }
+          }
         }
       }
     }
